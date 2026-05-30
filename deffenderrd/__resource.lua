9c9458ea2ed99dd2413401d496d3af70a3a485fa
fatrk3 <fatrk3@gmail.com>
2026-05-30 13:30:56 +0400

    Add files via upload

---
+++ deffenderrd/__resource.lua
@@ -0,0 +1,28 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+description 'Car DEFENDERDR Raidn'
+--[[ 
+    ██████╗  █████╗ ██╗██████╗ ███╗   ██╗
+    ██╔══██╗██╔══██╗██║██╔══██╗████╗  ██║
+    ██████╔╝███████║██║██║  ██║██╔██╗ ██║
+    ██╔══██╗██╔══██║██║██║  ██║██║╚██╗██║
+    ██║  ██║██║  ██║██║██████╔╝██║ ╚████║
+    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═══╝
+
+        © Raidn - All Rights Reserved
+        قابــل للنشــر | غيــر قابــل للبيــع
+        Publishable | Not For Sale
+        Discord: Raidn
+]] 
+
+files {
+    'carcols.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'handling.meta',
+}
+
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
